0486e8c34364ca090ec7bfe9585857143eeb39cd
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-08-30 18:40:50 +0200

    Add blur to background when mobile menu is active

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuButton = document.querySelector('.menu-btn');
 const menuList = document.querySelector('.menu-items');
 const menuItems = Array.from(document.querySelectorAll('.menu-item'));
 const menuIcon = document.querySelector('.menu-icon');
+const contentBody = document.querySelector('.content-body');
+const mediaQuery = window.matchMedia('(min-width: 992px)')
 
 menuButton.addEventListener('click', () => {
   menuList.classList.toggle('active');
@@ -9,15 +11,18 @@ menuButton.addEventListener('click', () => {
 
   if (menuButton.classList.contains('active')) {
     menuIcon.src = './images/Enable.svg';
+    contentBody.style.filter =  'blur(7px)';
   } else {
     location.reload();
+    contentBody.style.filter =  'none';
   }
 });
 
 menuItems.forEach((item) => {
   item.addEventListener('click', () => {
     menuList.classList.remove('active');
+    menuButton.classList.toggle('active');
     menuIcon.src = './images/Union.png';
+    contentBody.style.filter =  'none';
   });
 });
-
