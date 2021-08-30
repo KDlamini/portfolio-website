d6e9ed9ba78089258427980e17846d7dbd4739ae
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-08-30 15:44:20 +0200

    Add script file: Udpate css mobile and desktop menu items styling. add add event listeners for menu button and menu list.

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu-items');
+const menuItems = Array.from(document.querySelectorAll('.menu-item'));
+const menuIcon = document.querySelector('.menu-icon');
+
+menuButton.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+    menuButton.classList.toggle('active');
+    menuIcon.src="./image/close-btn.svg";
+});
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuList.classList.remove('active');
+        menuIcon.src="./images/Union.png";
+    })
+});
